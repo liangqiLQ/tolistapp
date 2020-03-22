9448b33e07c4f6b29d0040e840141c8efb5e6551
liangqiLQ <2608186018@qq.com>
2020-03-22 23:30:14 +0800

    未完待续

---
+++ functional_tests.py
@@ -0,0 +1,52 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+import unittest
+
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+
+        self.browser.get("http://localhost:8000")
+
+
+        self.assertIn('To-Do', self.browser.title)
+        headerText = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', headerText)
+
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'), "Enter a to-do item")
+
+
+        inputbox.send_keys('Buy feathers')
+        inputbox.send_keys(Keys.ENTER)
+
+        time.sleep(1)
+
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Clean feathers')
+        inputbox.send_keys(Keys.ENTER)
+
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy feathers', [row.text for row in rows])
+        self.assertIn('2: Clean feathers', [row.text for row in rows])
+
+
+        
+
+        self.fail("Finish the test")
+
+
+if __name__ == "__main__":
+    unittest.main(warnings='ignore')
